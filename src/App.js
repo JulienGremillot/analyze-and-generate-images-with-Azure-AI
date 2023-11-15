5f7d3a24da38a8904cf2aa0bfc86360333a5a3f2
GREMILLOT Julien <jgremillot@tibco.fr>
2023-11-15 15:47:06 +0100

    ajoute formulaire de base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // simple IHM with a title, a text input to enter an image URL and a button to submit the form
+  return (
+    <div className="App">
+      <h1>Image Analysis and Generation App</h1>
+      <form>
+        <div>
+          <label>Image URL or Prompt</label>
+          &nbsp;
+          <input type="text" />
+        </div>
+        <div>
+          <button>Analyze</button>
+          &nbsp;
+          <button>Generate</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
